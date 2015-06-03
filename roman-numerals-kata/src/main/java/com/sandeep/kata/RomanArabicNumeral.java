9bcb43ce3ac9e3f3fe6d87e90bad07ee3b7c15db
maharsandeep <mahar.sandeep@gmail.com>
2015-06-03 23:40:05 +0530

    Refactored code to remove switch statement and have more generic method

--- roman-numerals-kata/src/main/java/com/sandeep/kata/RomanArabicNumeral.java
+++ roman-numerals-kata/src/main/java/com/sandeep/kata/RomanArabicNumeral.java
@@ -5,42 +5,26 @@ public class RomanArabicNumeral {
 	private int numeral;
 	private String roman;
 
+	private static int[] numbers = { 1000, 500, 100, 50, 10, 5, 4, 1 };
+	private static String[] letters = { "M", "D", "C", "L", "X", "V", "IV", "I" };
+
 	public void setArabic(int num) {
 		numeral = num;
-		setRomanValue(num);
+		if (num < 1 || num > 3999) {
+			roman = "NOT-SUPPORTED";
+		} else {
+			setRomanValue(num);
+		}
 	}
 
 	private void setRomanValue(int num) {
-		switch (num) {
-		case 1:
-			roman = "I";
-			break;
-		case 3:
-			roman = "III";
-			break;
-		case 4:
-			roman = "IV";
-			break;
-		case 5:
-			roman = "V";
-			break;
-		case 10:
-			roman = "X";
-			break;
-		case 50:
-			roman = "L";
-			break;
-		case 100:
-			roman = "C";
-			break;
-		case 500:
-			roman = "D";
-			break;
-		case 1000:
-			roman = "M";
-			break;
-		default:
-			roman = "NOT-SUPPORTED";
+		roman = "";
+		int N = num;
+		for (int i = 0; i < numbers.length; i++) {
+			while (N >= numbers[i]) {
+				roman += letters[i];
+				N -= numbers[i];
+			}
 		}
 	}
 
