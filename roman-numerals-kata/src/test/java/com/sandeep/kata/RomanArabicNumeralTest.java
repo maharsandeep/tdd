c3a6bc9341ecadafebe1ba1e34ddb53a0dfd5b84
maharsandeep <mahar.sandeep@gmail.com>
2015-06-03 23:14:21 +0530

    First refactoring of test case

--- roman-numerals-kata/src/test/java/com/sandeep/kata/RomanArabicNumeralTest.java
+++ roman-numerals-kata/src/test/java/com/sandeep/kata/RomanArabicNumeralTest.java
@@ -1,13 +1,21 @@
 package com.sandeep.kata;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class RomanArabicNumeralTest {
 	
+	private RomanArabicNumeral ran;
+	
+	@Before
+	public void init(){
+		ran = new RomanArabicNumeral();
+	}
+	
 	@Test
 	public void checkIfLessThanArabicOneNumbersNotSupported(){
-		RomanArabicNumeral ran = new RomanArabicNumeral();
 		ran.setArabic(0);
 		assertEquals("NOT-SUPPORTED", ran.getRoman());
 		ran.setArabic(-1);
@@ -16,7 +24,6 @@ public class RomanArabicNumeralTest {
 	
 	@Test
 	public void checkIfGreaterThan3999NumbersNotSupported(){
-		RomanArabicNumeral ran = new RomanArabicNumeral();
 		ran.setArabic(4000);
 		assertEquals("NOT-SUPPORTED", ran.getRoman());
 		ran.setArabic(4001);
